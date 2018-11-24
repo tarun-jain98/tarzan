0c2a9e245400aee15dc8bd5c2477e6e1c75581f4
Tarun Jain <tarunjofficial@gmail.com>
2018-11-24 12:01:00 +0530

    commit2

---
+++ t1/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth.views import login, logout
+from . import forms
+
+app_name = 't1'
+
+urlpatterns = [
+url(r'^$', views.index, name="index-view"),
+]
