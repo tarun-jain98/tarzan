8763aae13c8e35baae90d285caad2f8119249303
Tarun Jain <tarunjofficial@gmail.com>
2018-11-24 12:06:20 +0530

    commit2

--- t1/admin.py
+++ t1/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
+from .models import *
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
 
 # Register your models here.
+
+admin.site.site_header = "Tarzan System";
+
+admin.site.register(Designation)
+
+admin.site.register(Department)
+
+# Register your models here.
+class UserResource(resources.ModelResource):
+	class Meta:
+		model = User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin, ImportExportModelAdmin):
+
+	fieldsets = (
+		(None, {'fields': ('username', 'email', 'password')}),
+		(('Personal info'), {'fields': ('first_name', 'last_name', 'phone', 'department','info')}),
+		(('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+									   )}),
+
+		(('Status'), {'fields': ('teach_status')}),
+
+		(('Designation'), {'fields': ('designation',)}),
+		(('Important dates'), {'fields': ('last_login', 'date_joined')}),
+	)
+
+	list_display = ('username', 'first_name', 'last_name', 'phone', 'email')
+	search_fields = ('email', 'first_name', 'last_name', 'username', 'phone')
+	ordering = ('username',)
+	read_only = "password"
+	resource_class = UserResource
--- t1/models.py
+++ t1/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
-# Create your models here.
+#TABLE-1
+
+class Designation(models.Model):
+	"""
+	Description: Holds the Designations avbailable
+	"""
+	name = models.CharField(max_length=20, blank=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+class Department(models.Model):
+	"""
+	Description: Model Description
+	"""
+	name = models.CharField(max_length=20, blank=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+class User(AbstractUser):
+	phone = models.BigIntegerField(null=True)
+	department = models.ForeignKey('Department', on_delete=models.CASCADE,null=True)
+	designation = models.ForeignKey('Designation', on_delete=models.CASCADE,null=True)
+	teach_status = models.BooleanField(default=False)
+	info = models.CharField(max_length=20, blank=True, null=True)
+
+
+	def __str__(self):
+		return self.username
+
+	def is_assistant_professor(self):
+		faculty = Designation.objects.get(pk=6)
+		faculty1 = str(faculty)
+		faculty2 = str(self.designation.name)
+		if faculty1 == faculty2:
+			return True
+		return False
+
+	def is_associate_professor(self):
+		faculty = Designation.objects.get(pk=5)
+		faculty1 = str(faculty)
+		faculty2 = str(self.designation.name)
+		if faculty1 == faculty2:
+			return True
+		return False
+
+	def is_professor(self):
+		faculty = Designation.objects.get(pk=4)
+		faculty1 = str(faculty)
+		faculty2 = str(self.designation.name)
+		if faculty1 == faculty2:
+			return True
+		return False
+
+	def is_hod(self):
+		faculty = Designation.objects.get(pk=3)
+		faculty1 = str(faculty)
+		faculty2 = str(self.designation.name)
+		if faculty1 == faculty2:
+			return True
+		return False
+
+	def is_principal(self):
+		faculty = Designation.objects.get(pk=2)
+		faculty1 = str(faculty)
+		faculty2 = str(self.designation.name)
+		if faculty1 == faculty2:
+			return True
+		return False
+
+	def is_ao(self):
+		faculty = Designation.objects.get(pk=1)
+		faculty1 = str(faculty)
+		faculty2 = str(self.designation.name)
+		if faculty1 == faculty2:
+			return True
+		return False
+
+	def get_designation(self):
+		return self.designation.name
